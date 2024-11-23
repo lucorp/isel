a6c41e54fc401584204463fe958eff54571fbd4f
Stefan Lucivjansky <stefan.lucivjansky@synpulse.com>
2024-11-23 12:44:49 +0100

    Updated build packages to newest verisons and channged build cycle little bit.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,42 +1,85 @@
+'use strict';
+
 module.exports = function(grunt) {
     grunt.initConfig({
-		sass: {
-			options: {
-                includePaths: ['node_modules/bootstrap-sass/assets/stylesheets']
-            },
+        sass: {
             dist: {
-				options: {
-					outputStyle: 'compressed'
-				},
-                files: [{
-                    'assets/css/isel.min.css': 'assets/css/isel.scss',
-				}]
+                options: {
+                    style: 'expanded'
+                },
+                files: {
+                    'assets/css/isel.css': 'assets/css/isel.scss',
+                }
             }
         },
         uglify: {
-          my_target: {
-            files: {
-               'assets/js/isel.min.js': ['assets/js/isel.js'],
-              }
+            dist: {
+                src: 'assets/js/isel.js',
+                dest: 'assets/js/isel.min.js'
+            },
+        },
+        watch: {
+            scripts: {
+                files: ['assets/js/isel.js'],
+                tasks: ['uglify'],
+                options: {
+                    spawn: false,
+                }
+            },
+            stylesheets: {
+                files: ['assets/css/isel.scss'],
+                tasks: ['sass'],
+                options: {
+                    spawn: false,
+                }
             }
         },
-        watcher: {
-            css: {
-                files: ['assets/js/*.js', 'assets/css/*.scss'],
-                tasks: ['sass', 'uglify'],
-                options: {
-                    livereload: true,
-                },
-            }
-
-        }
     });
-    grunt.loadNpmTasks("grunt-sass");
+
+    // Plugin tasks
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-watcher');
-    
-    grunt.registerTask("buildcss", ["sass"]);
-    grunt.registerTask("buildjs", ["uglify"]);
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task
+    grunt.registerTask('default', ['sass', 'uglify']);
+
 };
+
+// grunt.initConfig({
+// 	sass: {
+// 		options: {
+//             implementation: sass,
+//             sourceMap: true,
+//             includePaths: ['node_modules/bootstrap-sass/assets/stylesheets']
+//         },
+//         dist: {
+// 			options: {
+// 				outputStyle: 'compressed'
+// 			},
+//             files: [{
+//                 'assets/css/isel.min.css': 'assets/css/isel.scss',
+// 			}]
+//         }
+//     },
+//     uglify: {
+//       my_target: {
+//         files: {
+//            'assets/js/isel.min.js': ['assets/js/isel.js'],
+//           }
+//         }
+//     },
+//     watcher: {
+//         css: {
+//             files: ['assets/js/*.js', 'assets/css/*.scss'],
+//             tasks: ['sass', 'uglify'],
+//             options: {
+//                 livereload: true,
+//             },
+//         }
+
+//     }
+// });
+
 
 
